58693bd7abb871a120738822b336fa805529eb0f
ABrozovic <alejandro.brozovic@gmail.com>
2023-09-20 18:31:51 -0400

    feat: add lexer_test

---
+++ internal/lexer/lexer_test.go
@@ -0,0 +1,73 @@
+package lexer_test
+
+import (
+	"monkey-interpreter/internal/lexer"
+	"monkey-interpreter/internal/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+	x + y;
+	};
+	let result = add(five, ten);
+	`
+	tests := []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.Let, "let"},
+		{token.Ident, "five"},
+		{token.Assign, "="},
+		{token.Int, "5"},
+		{token.Semicolon, ";"},
+		{token.Let, "let"},
+		{token.Ident, "ten"},
+		{token.Assign, "="},
+		{token.Int, "10"},
+		{token.Semicolon, ";"},
+		{token.Let, "let"},
+		{token.Ident, "add"},
+		{token.Assign, "="},
+		{token.Function, "fn"},
+		{token.LParen, "("},
+		{token.Ident, "x"},
+		{token.Comma, ","},
+		{token.Ident, "y"},
+		{token.RParen, ")"},
+		{token.LBrace, "{"},
+		{token.Ident, "x"},
+		{token.Plus, "+"},
+		{token.Ident, "y"},
+		{token.Semicolon, ";"},
+		{token.RBrace, "}"},
+		{token.Semicolon, ";"},
+		{token.Let, "let"},
+		{token.Ident, "result"},
+		{token.Assign, "="},
+		{token.Ident, "add"},
+		{token.LParen, "("},
+		{token.Ident, "five"},
+		{token.Comma, ","},
+		{token.Ident, "ten"},
+		{token.RParen, ")"},
+		{token.Semicolon, ";"},
+		{token.EOF, ""},
+	}
+
+	l := lexer.New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
